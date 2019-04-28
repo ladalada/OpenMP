ab29de3f3fc6d32d2accf8b7b2ab04541435ced3
Lada Astashenkova <lada.astashenkova@yandex.ua>
2019-04-28 12:26:21 +0300

    Update 2.cpp

--- task_2/2.cpp
+++ task_2/2.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 int main() {
 	int n = 100000;
-	double DotProductSuccessively = 0.0;
-	double DotProductParallel = 0.0;
-	
+	double ScalarProductSuccessively = 0.0;
+	double ScalarProductParallel = 0.0;
+
 	vector<int> v1;
 	for (int i = 0; i < n; i++) {
 		int b = rand() % 10 + 1;
 		v1.push_back(b);
 	}
-	
+
 	vector<int> v2;
 	for (int i = 0; i < n; i++) {
 		int b = rand() % 10 + 1;
@@ -23,22 +23,22 @@ int main() {
 	//Successively
 	double t1 = omp_get_wtime();
 	for (int i = 0; i < n; i++) {
-		DotProductSuccessively += v1[i] * v2[i];
+		ScalarProductSuccessively += v1[i] * v2[i];
 	}
 	double t11 = omp_get_wtime();
 
-	cout << "DotProductSuccessively = " << DotProductSuccessively << "\n";
+	cout << "ScalarProductSuccessivelyy = " << ScalarProductSuccessively << "\n";
 	cout << "Running time in sequential mode = " << t11 - t1 << "\n";
 
 	//Parallel
 	double t2 = omp_get_wtime();
 	#pragma omp parallel for reduction(+:DotProductParallel)
 	for (int i = 0; i < n; i++) {
-		DotProductParallel += v1[i] * v2[i];
+		ScalarProductParallel += v1[i] * v2[i];
 	}
 	double t22 = omp_get_wtime();
 
-	cout << "DotProductParallel = " << DotProductParallel << "\n";
+	cout << "ScalarProductParallel = " << ScalarProductParallel << "\n";
 	cout << "Running time in parallel mode = " << t22 - t2 << "\n";
 
 	system("pause");
