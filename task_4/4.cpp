fd5b9f4c5d25322bcb215aaf6680559fe7bf98cd
Lada Astashenkova <lada.astashenkova@yandex.ua>
2019-04-28 09:48:57 +0300

    Create 4.cpp

---
+++ task_4/4.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <omp.h>
+using namespace std;
+
+int main() {
+	int i, j;
+	int n = 100, m = 100;
+
+	int** matrix1 = new int*[n];
+	for (i = 0; i < n; i++) {
+		matrix1[i] = new int[m];
+		for (j = 0; j < m; j++) {
+			matrix1[i][j] = rand() % 10 + 1;
+		}
+	}
+
+	int** matrix2 = new int*[m];
+	for (i = 0; i < m; i++) {
+		matrix2[i] = new int[n];
+		for (j = 0; j < n; j++) {
+			matrix2[i][j] = rand() % 10 + 1;
+		}
+	}
+
+	int** result1 = new int*[n];
+	for (int i = 0; i < n; i++) {
+		result1[i] = new int[n];
+	}
+
+	int** result2 = new int*[n];
+	for (int i = 0; i < n; i++) {
+		result2[i] = new int[n];
+	}
+
+	//Successively
+	double t1 = omp_get_wtime();
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			result1[i][j] = 0;
+			for (int k = 0; k < m; k++) {
+				result1[i][j] += matrix1[i][k] * matrix2[k][j];
+			}
+		}
+	}
+	double t11 = omp_get_wtime();
+
+	cout << "MatrixProductSuccessively = " << "\n";
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << result1[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "Running time in sequential mode = " << t11 - t1 << "\n";
+
+	//Parallel
+	double t2 = omp_get_wtime();
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			result2[i][j] = 0;
+#pragma omp parallel for reduction(+:result2[i][j])
+			for (int k = 0; k < m; k++) {
+				result2[i][j] += matrix1[i][k] * matrix2[k][j];
+			}
+		}
+	}
+	double t22 = omp_get_wtime();
+
+	cout << "MatrixProductParallel = " << "\n";
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << result2[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "Running time in parallel mode = " << t22 - t2 << "\n";
+
+	for (int i = 0; i < n; i++) {
+		delete[] matrix1[i];
+	}
+	delete[] matrix1;
+
+	for (int i = 0; i < m; i++) {
+		delete[] matrix2[i];
+	}
+	delete[] matrix2;
+
+	for (int i = 0; i < n; i++) {
+		delete[] result1[i];
+	}
+	delete[] result1;
+
+	for (int i = 0; i < n; i++) {
+		delete[] result2[i];
+	}
+	delete[] result2;
+
+	system("pause");
+	return 0;
+}
