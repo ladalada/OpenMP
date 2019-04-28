70b4492a509ad911212672946425bf505e69a2d5
Lada Astashenkova <lada.astashenkova@yandex.ua>
2019-04-28 09:53:04 +0300

    Update 5_block.cpp

--- task_5/5_block.cpp
+++ task_5/5_block.cpp
@@ -2,38 +2,30 @@
 #include <omp.h>
 using namespace std;
 
-int main()
-{
+int main() {
 	int max_threads_num = 10;
 	int max_n = 100000;
 
 	cout << "\n" << "Dimention" << "\t" << "Threads number" << "\t" << "Running time" << "\n\n";
 
-	for (int n = 15; n < max_n + 1; n *= 10)
-	{
+	for (int n = 15; n < max_n + 1; n *= 10) {
 		int** matrix = new int*[n];
-		for (int i = 0; i < n; i++)
-		{
+		for (int i = 0; i < n; i++) {
 			matrix[i] = new int[n];
-			for (int j = 0; j < n; j++)
-			{
-
+			for (int j = 0; j < n; j++) {
 				matrix[i][j] = rand() % 10;
 			}
 		}
 
 		int* vector = new int[n];
-		for (int i = 0; i < n; i++)
-		{
+		for (int i = 0; i < n; i++) {
 			vector[i] = rand() % 10;
 		}
 
-		for (int threads_num = 1; threads_num < max_threads_num + 1; threads_num += 2)
-		{
+		for (int threads_num = 1; threads_num < max_threads_num + 1; threads_num += 2) {
 			int* result = new int[n];
 			double t1 = omp_get_wtime();
-			for (int i = 0; i < n; i++)
-			{
+			for (int i = 0; i < n; i++) {
 				result[i] = 0;
 			}
 
@@ -42,14 +34,11 @@ int main()
 				int b = omp_get_num_threads();
 				int block_size = n / b;
 #pragma omp for
-				for (int block_num = 0; block_num < b * b; block_num++)
-				{
+				for (int block_num = 0; block_num < b * b; block_num++) {
 					int i = block_num / b;
 					int j = block_num % b;
-					for (int k = i * block_size; k < (i + 1) * block_size; k++)
-					{
-						for (int l = j * block_size; l < (j + 1) * block_size; l++)
-						{
+					for (int k = i * block_size; k < (i + 1) * block_size; k++) {
+						for (int l = j * block_size; l < (j + 1) * block_size; l++) {
 							result[k] += matrix[k][l] * vector[l];
 						}
 					}
@@ -64,8 +53,7 @@ int main()
 			delete[] result;
 		}
 
-		for (int i = 0; i < n; i++)
-		{
+		for (int i = 0; i < n; i++) {
 			delete[] matrix[i];
 		}
 		delete[] matrix;
